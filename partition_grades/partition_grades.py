e9ccc7fc5fb0af4f9236e2414524fc12f75d72e3
Seth Fiawoo <syfiawoo@gmail.com>
2020-07-10 14:06:30 -0500

    Add comments to solution

--- partition_grades/partition_grades.py
+++ partition_grades/partition_grades.py
@@ -1,9 +1,12 @@
 def partition_grades(grades, pass_grade):
-    next_pass_index = 0
+    next_pass_index = 0  # keep track of next index to be swapped
+    # go through all the grades
     for i in range(len(grades)):
+        # check if a passing grade was found
         if grades[i] >= pass_grade:
+            # swap the current passing grade with the next index to be swapped
             curr = grades[next_pass_index]
             grades[next_pass_index] = grades[i]
             grades[i] = curr
-            next_pass_index += 1
-    print(grades)
+            next_pass_index += 1  # advance to next position
+    print(f'Partitioned grades: {grades}')
